76ec3a840c9de4a3ded83fb1fd3b24b20958116a
Mayor Creator <paulaboagye@outlook.com>
2026-04-18 19:42:16 -0400

    add individual colors to the bar chart

--- nswl_season25_visual.py
+++ nswl_season25_visual.py
@@ -2,6 +2,23 @@ import csv
 
 from plotly import offline
 
+team_colors = {
+    "Angel City FC": "#F1B1A5",
+    "Chicago Stars": "#3AB5E8",
+    "Houston Dash": "#FF6900",
+    "Kansas City Current": "#CF3339",
+    "Gotham FC": "#A9F1FD",
+    "NC Courage": "#AB0033",
+    "Reign": "#292431",
+    "Orlando Pride": "#61259E",
+    "Portland Thorns": "#99242B",
+    "Racing Louisville": "#C5B5F2",
+    "San Diego Wave": "#032E62",
+    "Washington Spirit": "#000000",
+    "Royals": "#0E1735",
+    "Bay FC": "#0D2032",
+}
+
 filename = "./data/nswl_season_25_data.csv"
 with open(filename) as f:
     read_csv = csv.reader(f)
@@ -14,7 +31,15 @@ with open(filename) as f:
         goals_scored.append(int(row[1]))
 
     # Visual data
-    data = [{"type": "bar", "x": teams, "y": goals_scored}]
+    colors = [team_colors.get(team, "#888888") for team in teams]
+    data = [
+        {
+            "type": "bar",
+            "x": teams,
+            "y": goals_scored,
+            "marker": {"color": colors, "line": {"color": "black", "width": 1}},
+        }
+    ]
     layout = {
         "title": {
             "text": "Goals Scored Per Each Team",
